0789311239da3970d6aa75aebd1966f558639ce9
bodrus <maxshop2020@gmail.com>
2024-04-18 17:21:06 +0300

    feat: added Bottom Tab Navigator

---
+++ src/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import PostUploadScreen from '../screens/PostUploadScreen';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import colors from '../theme/colors.ts';
+import HomeStackNavigator from './HomeStackNavigator.tsx';
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.primary,
+      }}>
+      <Tab.Screen
+        name="HomeStack"
+        component={HomeStackNavigator}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="home-filled" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialIcons name="search" size={size} color={color} />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Upload"
+        component={PostUploadScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="plus-circle-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="heart-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="MyProfile"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="user-circle-o" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
