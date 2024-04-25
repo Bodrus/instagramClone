49ad6f0d4c053ebb68a7b1e8e66feabfa13c7425
bodrus <maxshop2020@gmail.com>
2024-04-25 21:06:49 +0300

    feat: setup aws amplify

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Navigation from './src/navigation';
 
+import {Amplify} from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
+
+Amplify.configure(amplifyconfig);
+
 if (__DEV__) {
   require('./ReactotronConfig');
 }
